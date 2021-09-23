e0ae2b2e69177681f713e459f7cd0f9db4e021d5
Nick Aquina <nickaquina@gmail.com>
2021-09-23 22:23:17 +0200

    Remove previous netlink dump before starting WireGuard

--- doc/dump-netlink.sh
+++ doc/dump-netlink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo ip link add  nlmon0 type nlmon
 sudo ip link set dev nlmon0 up
+sudo rm netlink.pcap
 touch netlink.pcap
 wireshark -Y '!nl80211 && !netlink-route' -k -i <(tail -f -c +0 netlink.pcap) &
-sleep 1
 sudo tcpdump -i nlmon0 -w netlink.pcap
